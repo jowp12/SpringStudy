065bb94e7709ed93cbe7668d31e27037d2e448db
jowp12 <664668524@qq.com>
2019-06-15 00:11:40 +0800

    add comment

--- src/main/java/com/penghui/beanFacoryPostProcessorTest/Main.java
+++ src/main/java/com/penghui/beanFacoryPostProcessorTest/Main.java
@@ -4,6 +4,9 @@ import com.penghui.beanFacoryPostProcessorTest.entity.User;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+/**
+ * 添加注释
+ */
 public class Main {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
